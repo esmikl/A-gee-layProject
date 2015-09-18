3ff8314fe6cde72dd5e2d8758c31def4e48bb3ea
Emily Mikl <emilysmikl@gmail.com>
2015-09-18 17:39:49 -0500

    Change tag field to text field instead of text area. Modify database to take in string instead of text for tag field.

--- db/migrate/20150918222029_create_notes.rb
+++ db/migrate/20150918222029_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
       t.string :title
-      t.text :tag
+      t.string :tag
       t.text :content
 
       t.timestamps null: false
