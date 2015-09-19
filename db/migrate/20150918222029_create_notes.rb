4f77925afe403ef497fd0c5fdd18fb4439e57042
Emily Mikl <emilysmikl@gmail.com>
2015-09-19 16:54:05 -0500

    Add scrawl image to landing page, edit css, change tag field in database to text.

--- db/migrate/20150918222029_create_notes.rb
+++ db/migrate/20150918222029_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
       t.string :title
-      t.string :tag
+      t.text :tag
       t.text :content
 
       t.timestamps null: false
