da5cd536a0bd696ee1b3c93945b60c446b317e97
Edward Dumholt <edumholt@gmail.com>
2015-09-30 09:02:15 -0500

    ran migration to add slug to notes model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150918222029) do
+ActiveRecord::Schema.define(version: 20150930135825) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150918222029) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
 
 end
