fb6f53dec5045d28dc38e00fc49e5e7615f89439
Edward Dumholt <edumholt@gmail.com>
2015-10-02 10:16:52 -0500

    tracking new db/migrate files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001055641) do
+ActiveRecord::Schema.define(version: 20151002150420) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
