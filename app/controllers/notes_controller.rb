2eef235a3265627faf1015abc00c5cf8f5d236c2
Emily Mikl <emilysmikl@gmail.com>
2015-10-05 16:48:33 -0500

    Trying to fix query.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,7 +4,7 @@ class NotesController < ApplicationController
   # GET /notes
   # GET /notes.json
   def index
-    @notes = Note.where(:trash => false)
+    @notes = Note.all.where(:trash => "false")
     if params[:q]
       @notes = Note.find_all_by_query(params[:q])
     else
