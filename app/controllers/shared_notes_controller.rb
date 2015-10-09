1de0c5fae16d2335a1007a2177e69195d090463f
Edward Dumholt <edumholt@gmail.com>
2015-10-09 14:40:41 -0500

    Refined page layouts, styled links

--- app/controllers/shared_notes_controller.rb
+++ app/controllers/shared_notes_controller.rb
@@ -6,7 +6,7 @@ class SharedNotesController < ApplicationController
     if @note == nil
       render 'shared_notes/no_shared_notes'
     else
-      render :show
+      render :edit
     end
   end
   
