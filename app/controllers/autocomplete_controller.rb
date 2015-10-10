0b7186a64ed28d2ebd3320f1485728e1e444c471
Emily Mikl <emilysmikl@gmail.com>
2015-10-10 16:14:04 -0500

    Start implementing autocomplete

--- app/controllers/autocomplete_controller.rb
+++ app/controllers/autocomplete_controller.rb
@@ -1,5 +1,6 @@
 class AutocompleteController < ApplicationController
-  def autocomplete_tag_name
-    @tags = ActsAsTaggableOn::Tag.all.map{|t| t.name}.to_json
-  end
+  autocomplete :tag, :name
+  #def autocomplete_tag_name
+    #@tags = ActsAsTaggableOn::Tag.all.map{|t| t.name}.to_json
+  #end
 end
