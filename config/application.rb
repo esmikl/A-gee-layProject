ba40bba388e75be9315a3a959845fbd4b8463ce4
Edward Dumholt <edumholt@gmail.com>
2015-10-11 09:10:13 -0500

    Added password reset feature

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module Scrawl
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    ActionMailer::Base.smtp_settings = {
+      :address          => 'smtp.gmail.com',
+      :domain           => 'mail.google.com',
+      :port              => 587,
+      :user_name      => "edumholt@gmail.com",
+      :password        => "husker333",
+      :authentication  => 'login',
+      :enable_starttls_auto => true
+      }
+    
   end
 end
